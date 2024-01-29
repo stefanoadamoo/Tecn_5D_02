b5747328c05e56b323f7a2646f2ac6d12b98fc3a
stefanoadamoo <adamo.stefano@iisgalvanimi.edu.it>
2024-01-29 08:40:43 +0000

    Spotify Completato

--- Spotify/spotify2/src/app/spotify.service.ts
+++ Spotify/spotify2/src/app/spotify.service.ts
@@ -14,7 +14,7 @@ export class SpotifyService {
     const url = `https://api.spotify.com/v1/search?q=${query}&type=track`;
     const headers = new HttpHeaders({
       Authorization:
-        'Bearer BQDqMwWA2qQ9uaXvdutXPQYYyEnteT8wSEGrpcLlCszeu74k9zhiFnFg1QN6-JOwV6xCtGjpmuQJcdTaRGN9_HufxfougpRX3O-Bmr_kkhf8iqjLp5WmaKip6pVUNz1sOdfVN9X1w_q1TNAG3jwDON99PzQI0oHb2_L36bhOcetC_2j0QgvGxS8Rh55Y81L0G5I'
+        'Bearer BQCgWJPxPfEgaukYyoUEXE6LsPsSE1Oy0BItrcSOsAg2Ykk89pba2DD58kPI-KnHkmH9565Rl2jbi8K8wT48x7yDKvI-T0qvwwtycAQraCq4sbdNnrS2lW5zd3xAjpcEX3aGfmUhRnPhK-lCDt5n4Okv1cETOcjBpvQZ8U2ob9O-FpYbr8Ec9YPbvKD3o3UPJzA'
     });
 
     let obsTracks = this.http.get(url, { headers });
