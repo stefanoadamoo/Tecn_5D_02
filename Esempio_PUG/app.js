004ed20601894ca152d80cc33a40530a62dd9bd4
stefanoadamoo <adamo.stefano@iisgalvanimi.edu.it>
2024-02-05 10:03:39 +0000

    Esercitazione PUG 05/02/2024

---
+++ Esempio_PUG/app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+app.set('view engine', 'pug'); 
+app.use(express.static(__dirname + '/public')); 
+app.get('/', function (req, res) {
+
+res.render('index'); 
+});
+app.listen(3000, function () {
+console.log('Example app listening on port 3000!');
+});
