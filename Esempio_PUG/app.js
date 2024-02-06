3956d95028206cf908232dd2c248edee5e419607
stefanoadamoo <adamo.stefano@iisgalvanimi.edu.it>
2024-02-06 12:51:25 +0000

    PUG 2 Parte Completata

--- Esempio_PUG/app.js
+++ Esempio_PUG/app.js
@@ -1,11 +1,16 @@
 var express = require('express');
+const people = require('./people.json');
 var app = express();
 app.set('view engine', 'pug'); 
 app.use(express.static(__dirname + '/public')); 
 app.get('/', function (req, res) {
 
-res.render('index'); 
-});
+    res.render('index', {
+    title: 'Homepage',
+    people: people.profiles,
+    content : 'Questa pagina parla del mondo e di tanto altro'
+    });
+    });
 app.listen(3000, function () {
 console.log('Example app listening on port 3000!');
 });
