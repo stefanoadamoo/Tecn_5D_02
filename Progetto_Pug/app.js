381117bf8dcb67224c7c1bba760402dad9c39d98
stefanoadamoo <adamo.stefano@iisgalvanimi.edu.it>
2024-02-26 21:18:53 +0000

    Pug Completato

--- Progetto_Pug/app.js
+++ Progetto_Pug/app.js
@@ -1,10 +1,42 @@
-var express = require('express');
-var app = express();
-app.set('view engine', 'pug'); //Dico a express di usare pug come motore di template
-app.get('/', function (req, res) {
-//res.send('Ciao Mondo');
-res.render('index'); //Dico a express di processare e inviare la pagina index.pug
+const express = require('express');
+const PItaliani = require('./json/PItaliani.json'); 
+const PFrancesi = require('./json/PFrancesi.json');
+const PSpagnoli = require('./json/PSpagnoli.json');
+
+const app = express();
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/Public'));
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+    title: 'Homepage',
+    ita: PItaliani.italiani,
+    fra: PFrancesi.francesi,
+    spa: PSpagnoli.spagnoli
+    });
 });
+
+app.get('/italiani', (req, res) => {
+    res.render('piloti', {
+    title: 'Piloti Italiani',
+    pil: PItaliani.italiani
+    });
+});
+
+app.get('/francesi', (req, res) => {
+    res.render('piloti', {
+        title: 'Piloti Francesi',
+        pil: PFrancesi.francesi
+    });
+});
+app.get('/spagnoli', (req, res) => {
+    res.render('piloti', {
+        title: 'Piloti Spagnoli',
+        pil: PSpagnoli.spagnoli
+    });
+});
+
 app.listen(3000, function () {
 console.log('Example app listening on port 3000!');
 });
